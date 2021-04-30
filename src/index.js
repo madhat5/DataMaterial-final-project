3ed7ad9e5806399e33cf021a1911fa3378832c52
madhat5 <davidmalary@gmail.com>
2021-04-30 19:27:41 -0400

    project done

--- src/index.js
+++ src/index.js
@@ -8,22 +8,34 @@ import data from './nyc2020Weather.json'
 const synth = new Tone.Synth().toDestination()
 const now = Tone.now()
 
-function playSynth() {
-    // synth.triggerAttackRelease("C2", "8n");
+function playSynth(count) {
+    synth.triggerAttackRelease(data[count].spnNote, "8n");
 
-    data.forEach((el, i) => {
-        // console.log(el.spnNote)
-        let note = el.spnNote
+    // data.forEach((el, i) => {
+    //     // console.log(el.spnNote)
+    //     let note = el.spnNote
 
-        synth.triggerAttackRelease(note, "8n", now + 1);
-    })
+    //     synth.triggerAttackRelease(note, "8n", now + 1);
+    // })
 }
-/**
- * Play Controls
- */
-
 
 document.querySelector("body > button").addEventListener("click", () => {
-    // console.log('blah')
-    playSynth();
+    // console.log(data[0].spnNote)
+    // playSynth();
+    myLoop();
 });
+
+
+var i = 1; //  set your counter to 1
+
+function myLoop() { //  create a loop function
+    setTimeout(function () { //  call a 3s setTimeout when the loop is called
+        // console.log('hello'); //  your code here
+        playSynth(i);
+        i++; //  increment the counter
+        if (i < data.length - 1) { //  if the counter < 10, call the loop function
+            myLoop(); //  ..  again which will trigger another 
+        } //  ..  setTimeout()
+    }, 250)
+}
+
